4a9c8254c57b33fccaf312233162436a33512a15
Vlad Vovk <wolfykey@gmail.com>
2020-04-14 23:35:57 +0300

    init

---
+++ src/components/about/AboutPage.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const AboutPage = () => {
+  return (
+    <div className="jumbotron">
+      <h1>About</h1>
+      <p>
+        React, Redux and Router Lorem ipsum dolor sit amet, consectetur
+        adipisicing elit. Porro, perspiciatis?
+      </p>
+    </div>
+  );
+};
+
+export default AboutPage;
