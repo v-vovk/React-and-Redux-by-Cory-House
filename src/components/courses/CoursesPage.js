d8d2fef5e421863ae30b656880bc0a4c0a9d8f6a
Vlad Vovk <wolfykey@gmail.com>
2020-04-15 22:05:57 +0300

    add boilerplate

---
+++ src/components/courses/CoursesPage.js
@@ -0,0 +1,39 @@
+import React, { Component } from "react";
+
+class CoursesPage extends Component {
+  state = {
+    course: {
+      title: ""
+    }
+  };
+
+  handleChange = (e) => {
+    const course = {
+      ...this.state.course,
+      title: e.target.value
+    };
+    this.setState({ course });
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(this.state.course.title);
+  };
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <h2>Courses</h2>
+        <h3>Add course</h3>
+        <input
+          type="text"
+          onChange={this.handleChange}
+          value={this.state.course.title}
+        />
+        <input type="submit" value="Save" />
+      </form>
+    );
+  }
+}
+
+export default CoursesPage;
